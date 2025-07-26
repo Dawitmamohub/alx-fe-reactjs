1803ec8948ad55c4ce0a5b7cac045cb54f7a9167
Dawit Mamo <Dawitmamoyou@gmail.com>
2025-07-26 12:54:39 +0300

    Add detailed recipe management: view, edit, delete, with routing

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,12 @@
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
 
-function App() {
-  return (
-    <div style={{ padding: '2rem' }}>
-      <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
-  );
-}
+const App = () => (
+  <div style={{ padding: '2rem' }}>
+    <h1>Recipe Sharing App</h1>
+    <AddRecipeForm />
+    <RecipeList />
+  </div>
+);
 
 export default App;
