2552c3514ed1efea0fb14e18479f19a72f45f996
Dawit Mamo <Dawitmamoyou@gmail.com>
2025-07-26 16:17:04 +0300

    Zustand updated

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
+import SearchBar from './components/SearchBar';
 
 const App = () => {
   return (
     <Router>
       <h1>Recipe Sharing App</h1>
       <AddRecipeForm />
+      <SearchBar />
       <Routes>
         <Route path="/" element={<RecipeList />} />
         <Route path="/recipes/:id" element={<RecipeDetails />} />
--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -2,7 +2,12 @@ import { Link } from 'react-router-dom';
 import { useRecipeStore } from './recipeStore';
 
 const RecipeList = () => {
-  const recipes = useRecipeStore((state) => state.recipes);
+  const recipes = useRecipeStore((state) => state.filteredRecipes.length > 0 || state.searchTerm ? state.filteredRecipes : state.recipes);
+  const searchTerm = useRecipeStore((state) => state.searchTerm);
+
+  if (recipes.length === 0 && searchTerm) {
+    return <div>No recipes found.</div>;
+  }
 
   return (
     <div>
