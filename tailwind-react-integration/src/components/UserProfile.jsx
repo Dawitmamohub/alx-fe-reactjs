17a2831d347ecf3bed3e8c2ddc858b719fe0ae62
Dawit Mamo <Dawitmamoyou@gmail.com>
2025-08-17 11:27:19 +0300

    fix: correct responsive classes for UserProfile

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -3,41 +3,34 @@ function UserProfile() {
     <div
       className="
         bg-gray-100
-        p-4 md:p-8
-        max-w-xs md:max-w-sm
+        sm:p-4 md:p-8
+        sm:max-w-xs md:max-w-sm
         mx-auto my-20
         rounded-lg shadow-lg
-        hover:shadow-xl
-        transition-shadow duration-300
       "
     >
       <img
         src="https://via.placeholder.com/150"
         alt="User"
         className="
-          w-24 h-24 md:w-36 md:h-36
+          sm:w-24 sm:h-24 md:w-36 md:h-36
           rounded-full
           mx-auto
-          transition-transform duration-300 ease-in-out
-          hover:scale-110
         "
       />
       <h1
         className="
-          text-lg md:text-xl
+          sm:text-lg md:text-xl
           text-blue-800
-          hover:text-blue-500
-          font-semibold
-          text-center
           my-4
-          transition-colors duration-300
+          text-center
         "
       >
         John Doe
       </h1>
       <p
         className="
-          text-sm md:text-base
+          sm:text-sm md:text-base
           text-gray-600
           text-center
         "
