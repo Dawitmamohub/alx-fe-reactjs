9c342cb4d190e9b71a34dbaf5bd7907a24648be2
Dawit Mamo <Dawitmamoyou@gmail.com>
2025-09-09 16:02:11 +0300

    error handling

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,43 +1,45 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import BlogPost from "./pages/BlogPost";
-import Profile from "./pages/Profile";
+import Profile from "./components/Profile";
 import NotFound from "./pages/NotFound";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
-    <div>
-      <nav style={{ marginBottom: "20px" }}>
-        <Link to="/">Home</Link> |{" "}
-        <Link to="/about">About</Link> |{" "}
-        <Link to="/profile">Profile</Link> |{" "}
-        <Link to="/blog/1">Blog Post #1</Link>
-      </nav>
+    <BrowserRouter>
+      <div>
+        <nav style={{ marginBottom: "20px" }}>
+          <Link to="/">Home</Link> |{" "}
+          <Link to="/about">About</Link> |{" "}
+          <Link to="/profile">Profile</Link> |{" "}
+          <Link to="/blog/1">Blog Post #1</Link>
+        </nav>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
 
-        {/* Dynamic route for blog posts */}
-        <Route path="/blog/:id" element={<BlogPost />} />
+          {/* Dynamic route for blog posts */}
+          <Route path="/blog/:id" element={<BlogPost />} />
 
-        {/* Protected route for profile */}
-        <Route
-          path="/profile/*"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        />
+          {/* Protected route for profile */}
+          <Route
+            path="/profile/*"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
 
-        {/* Fallback for unknown routes */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>
+          {/* Fallback for unknown routes */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 
-import React from "react";
-import { Navigate } from "react-router-dom";
-
 const isAuthenticated = false; // Set to false to test redirect behavior
 
 function ProtectedRoute({ children }) {
